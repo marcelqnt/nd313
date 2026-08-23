49690fabf350d64f85fa52909e02217bafd52420
Marcel Kuhnt <mkuhnt@oriolus-software.de>
2026-08-23 16:31:32 +0200

    empfaengt Almex-Avlb

--- src/interface/messages.rs
+++ src/interface/messages.rs
@@ -1,7 +1,8 @@
 use lotus_extra::{
     bb_system::{DomainMessageInterface, basic::BackBone},
-    messages,
+    messages::{self, handle_message},
 };
+use lotus_script::var::set_var;
 
 use crate::{Backbone, Modules};
 
@@ -15,10 +16,14 @@ impl DomainMessageInterface<Modules, Backbone> for Nd313MessageInterface {
         backbone: &mut Backbone,
         msg: &lotus_script::message::Message,
     ) -> bool {
-        msg.handle(|g: messages::std::AutomaticGearboxCurrentGear| {
+        handle_message(msg, |g: messages::std::AutomaticGearboxCurrentGear| {
             backbone.cockpit.vdv_dashboard.current_gear.set(g);
-            Ok(())
+            true
+        }) || handle_message(msg, |available: messages::almex::Available| {
+            if available.0 {
+                set_var("HasAlmex", true);
+            }
+            true
         })
-        .is_ok()
     }
 }
